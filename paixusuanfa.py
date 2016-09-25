b4ec154e0501ee28c710a3fc2cafb17f9442f6a3
kaixinjiuhao6654 <1157983684@qq.com>
2016-09-25 19:28:22 +0800

    Update paixusuanfa.py

--- paixusuanfa.py
+++ paixusuanfa.py
@@ -3,7 +3,7 @@
 
 def maopao(a):
 
-    for i in range(len(a) - 1, 1, -1):
+    for i in range(len(a) - 1, 0, -1):
         for j in range(i):
             if a[j] > a[j + 1]:
                 a[j], a[j + 1] = a[j + 1], a[j]
