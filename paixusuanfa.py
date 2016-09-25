8779fc2190e63aac48cd927fab55396b310da30c
wangpf <1157983684@qq.com>
2016-09-25 17:08:09 +0800

    排序算法

---
+++ paixusuanfa.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+
+def maopao(a):
+
+    for i in range(len(a) - 1, 1, -1):
+        for j in range(i):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+    print a
+
+a = [2, 1, 9, 4, 3, 5, 7, 6]
+maopao(a)
